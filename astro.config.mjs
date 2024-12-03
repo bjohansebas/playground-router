8338787d4a68076e374f1a8a0f8a6275bff79d4a
Sebastian Beltran <bjohansebas@gmail.com>
2024-12-02 22:29:43 -0500

    build: setup base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bjohansebas.github.io',
+  base: 'playground-router',
   integrations: [tailwind()]
 });
