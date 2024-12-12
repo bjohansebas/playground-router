db2b725f9896dac915a0b972399761f98190e6b7
Sebastian Beltran <bjohansebas@gmail.com>
2024-12-12 14:42:51 -0500

    use prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bjohansebas.github.io',
-  base: 'playground-router',
-  integrations: [tailwind()]
+  site: "https://bjohansebas.github.io",
+  base: "playground-router",
+  integrations: [tailwind()],
 });
